050f56f3115e201d7ca4bdbc0bcfb125800b8949
Arman_Samiei <samiei.arman@gmail.com>
2021-02-02 20:45:09 +0330

    add check_if_one_player_win function to check if a player has won the game

--- tic_toc_toe.py
+++ tic_toc_toe.py
@@ -6,7 +6,32 @@ def print_board(board):
                 continue
             else:
                 print(board[row*3+j].upper())
-
+def check_if_one_player_win(player_symbol, board, number_selected):
+    row = int(number_selected/3)
+    col = int(number_selected%3)
+    for i in range(2):
+        if (board[row*3+i]) != (board[row*3 + i + 1]):
+            break
+    else:
+        return True
+    for i in range(2):
+        if(board[i*3+col])!=(board[i*3+col+1]):
+            break
+    else:
+        return True
+    if row == col:
+        for i in range(2):
+                if(board[i][i])!=(board[i+1][i+1]):
+                    break
+        else:
+            return True
+    if row==col:
+        for i in range(2):
+            if(board[i][2-i]) != (board[i+1][1-i]):
+                break
+        else:
+            return True
+    return False
 
 
 

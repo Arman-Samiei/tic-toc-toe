fa5d6d1405d1efbe69a7374ef3e7eec0d8eafcd6
Arman_Samiei <samiei.arman@gmail.com>
2021-02-02 20:10:13 +0330

    add print_board function to print board

--- tic_toc_toe.py
+++ tic_toc_toe.py
@@ -1,1 +1,12 @@
+def print_board(board):
+    row = 2
+    while row >= 0:
+        for j in range(3):
+            if board[row*3+j] == '#':
+                continue
+            else:
+                print(board[row*3+j].upper())
+
+
+
 
